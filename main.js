4b1970e2d76cf0600e9352f5a1c9997d5ab96afd
Natalie Rodriguez <codinggeographer@gmail.com>
2023-07-27 17:08:48 -0600

    Fix color box button bug: Refactor JS and CSS

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Data Model
-var currentPalette = [];
+const currentPalette = [];
 
 // DOM Query Selectors
-var colorBoxContainer = document.querySelector(".color-box-container");
-var colorButton = document.querySelector(".color-button");
+const colorBoxContainer = document.querySelector(".color-box-container");
+const colorButton = document.querySelector(".color-button");
 
 // DOM Event Listeners
 window.addEventListener("load", generateNewPalette);
@@ -12,45 +12,42 @@ colorBoxContainer.addEventListener('click', toggleLock);
 
 // JavaScript Functions
 function generateRandomHexCode() {
-  var characters = "0123456789ABCDEF";
-  var hexCode = "#";
+  const characters = "0123456789ABCDEF";
+  let hexCode = "#";
   for (let i = 0; i < 6; i++) {
-  hexCode += characters[Math.floor(Math.random() * characters.length)];
+    hexCode += characters[Math.floor(Math.random() * characters.length)];
   }
   return hexCode;
 }
 
+function createColorBoxHTML(hexCode, locked) {
+  return `
+    <article class="color-box">
+      <figure class="color-box-figure" style="background-color: ${hexCode}">
+        <div class="lock-icon ${locked ? 'locked-icon' : 'unlocked-icon'}"></div>
+      </figure>
+      <figcaption>${hexCode}</figcaption>
+    </article>
+  `;
+}
+
 function generateNewPalette() {
-  currentPalette = [];
+  currentPalette.length = 0;
   colorBoxContainer.innerHTML = '';
   for (let i = 0; i < 5; i++) {
-  var randomHexCode = generateRandomHexCode();
-  currentPalette.push({ hexCode: randomHexCode, locked: false, id: i });
-  colorBoxContainer.innerHTML += `
-  <article class="color-box">
-    <figure class="color-box${i}" style="background-color: ${randomHexCode}">
-      <button>
-        <i class="lock-icon unlocked-icon" style="display:block"></i>
-        <i class="lock-icon locked-icon" style="display:none"></i>
-      </button>
-    </figure>
-    <figcaption>${randomHexCode}</figcaption>
-  </article>
-  `;
+    const randomHexCode = generateRandomHexCode();
+    currentPalette.push({ hexCode: randomHexCode, locked: false, id: i });
+    colorBoxContainer.innerHTML += createColorBoxHTML(randomHexCode, false, i);
   }
 }
 
 function toggleLock(event) {
-  if (event.target.classList.contains('lock-icon')) {
-    const lock = event.target;
-    
-    // Toggle between locked and unlocked states
-    if (lock.classList.contains('unlocked-icon')) {
-      lock.classList.remove('unlocked-icon');
-      lock.classList.add('locked-icon');
-    } else if (lock.classList.contains('locked-icon')) {
-      lock.classList.remove('locked-icon');
-      lock.classList.add('unlocked-icon');
-    }
+  const lockIcon = event.target;
+  if (lockIcon.classList.contains('lock-icon')) {
+    const colorBox = lockIcon.closest('.color-box');
+    const colorBoxIndex = Array.from(colorBoxContainer.children).indexOf(colorBox);
+    currentPalette[colorBoxIndex].locked = !currentPalette[colorBoxIndex].locked;
+    lockIcon.classList.toggle('locked-icon');
+    lockIcon.classList.toggle('unlocked-icon');
   }
 }
